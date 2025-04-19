5332b10fa69ea56028e6d665e12dd3749494ff9a
oettaqui <ossamaettaqui@gmail.com>
2025-04-19 13:17:03 +0100

    The last version with same upades to make sure all the services are work successefully

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-
-service mariadb start
-
-mariadb -v -u root << EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
+if [ -d "/var/lib/mysql/mysql" ] && [ -f "/var/lib/mysql/.initialized" ]; then
+    exec "$@"
+else
+    service mariadb start
+    sleep 10
+    mariadb -v -u root << EOF
+CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PASS_ROOT}';
+FLUSH PRIVILEGES;
 EOF
 
-sleep 5
-
-service mariadb stop
-
-exec $@ 
+    touch /var/lib/mysql/.initialized
+    service mariadb stop
+    exec "$@"
+fi
